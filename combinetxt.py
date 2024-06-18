bc2fcaba422f3c3677801a995d29cff627527df0
mtabarikasif <79154025+m-tabarik@users.noreply.github.com>
2024-06-18 21:13:13 +0500

    Create combinetxt.py

---
+++ combinetxt.py
@@ -0,0 +1,11 @@
+import os
+
+# Get the current working directory
+current_directory = os.getcwd()
+output_file = 'all.txt'
+
+with open(output_file, 'w') as outfile:
+    for filename in os.listdir(current_directory):
+        if filename.endswith('.txt'):
+            with open(os.path.join(current_directory, filename), 'r') as infile:
+                outfile.write(infile.read() + '\n')
